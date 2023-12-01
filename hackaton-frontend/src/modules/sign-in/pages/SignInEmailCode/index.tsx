332c0042a9927831088d956f4fa46c5839ba7ece
Michal Chruscielski <michalch775@gmail.com>
2023-12-01 22:16:57 +0100

    struct

---
+++ hackaton-frontend/src/modules/sign-in/pages/SignInEmailCode/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./styles.module.scss";
+
+function SignInEmailCode() {
+    return (
+        <div className={styles.div}>
+
+        </div>
+    );
+}
+
+export default SignInEmailCode;
